d4dabc76e3a16d2bbbbbf63cf410f12f1f62434d
Andy Craze <accraze@gmail.com>
2023-06-08 21:47:45 -0700

    add all card lists and message defun

--- tarock.el
+++ tarock.el
@@ -22,7 +22,77 @@
 ;;;
 (require 'cl-lib)
 
-(defvar tarot-cards
+(defvar swords-cards
+  '(("Ace of Swords" . "Clarity, truth, decision making.")
+    ("Two of Swords" . "Indecision, stalemate, avoidance.")
+    ("Three of Swords" . "Heartbreak, betrayal, emotional pain.")
+    ("Four of Swords" . "Rest, contemplation, healing.")
+    ("Five of Swords" . "Defeat, failure, conflict.")
+    ("Six of Swords" . "Moving forward, transition, leaving the past behind.")
+    ("Seven of Swords" . "Deception, trickery, cunning.")
+    ("Eight of Swords" . "Feeling trapped, helpless, limited.")
+    ("Nine of Swords" . "Fear, anxiety, insomnia.")
+    ("Ten of Swords" . "Pain, betrayal, defeat.")
+    ("Page of Swords" . "Intelligence, communication, swiftness.")
+    ("Knight of Swords" . "Boldness, assertiveness, directness.")
+    ("Queen of Swords" . "Intelligence, independence, honesty.")
+    ("King of Swords" . "Authority, power, rationality.")))
+
+(defvar pentacles-cards
+  '(("Ace of Pentacles" . "Opportunity, prosperity, abundance.")
+    ("Two of Pentacles" . "Balance, adaptability, time management.")
+    ("Three of Pentacles" . "Collaboration, teamwork, skilled work.")
+    ("Four of Pentacles" . "Hoarding, possessiveness, materialism.")
+    ("Five of Pentacles" . "Hardship, poverty, isolation.")
+    ("Six of Pentacles" . "Charity, generosity, sharing.")
+    ("Seven of Pentacles" . "Patience, perseverance, hard work.")
+    ("Eight of Pentacles" . "Diligence, skill, craftsmanship.")
+    ("Nine of Pentacles" . "Self-sufficiency, luxury, independence.")
+    ("Ten of Pentacles" . "Wealth, family, inheritance.")
+    ("Page of Pentacles" . "Manifestation, inspiration, new ideas.")
+    ("Knight of Pentacles" . "Dependability, stability, practicality.")
+    ("Queen of Pentacles" . "Nurturing, practicality, abundance.")
+    ("King of Pentacles" . "Wealth, abundance, financial security.")))
+
+(defvar cups-cards
+  '(("Ace of Cups" . "Emotional new beginnings, new relationships.")
+    ("Two of Cups" . "Love, connection, partnership.")
+    ("Three of Cups" . "Celebration, friendship, community.")
+    ("Four of Cups" . "Apathy, contemplation, reevaluation.")
+    ("Five of Cups" . "Loss, grief, disappointment.")
+    ("Six of Cups" . "Nostalgia, memories, childhood.")
+    ("Seven of Cups" . "Opportunities, choices, decision-making.")
+    ("Eight of Cups" . "Abandonment, disappointment, moving on.")
+    ("Nine of Cups" . "Happiness, satisfaction, fulfillment.")
+    ("Ten of Cups" . "Happy family life, emotional fulfillment.")
+    ("Page of Cups" . "Creative inspiration, intuition, sensitivity.")
+    ("Knight of Cups" . "Romance, charm, emotional depth.")
+    ("Queen of Cups" . "Empathy, compassion, emotional support.")
+    ("King of Cups" . "Emotional balance, maturity, calmness.")))
+
+(defvar wands-cards
+  '(("Ace of Wands" . "Inspiration, new beginnings, potential.")
+    ("Two of Wands" . "Planning, making decisions, leaving home.")
+    ("Three of Wands" . "Expansion, growth, opportunity.")
+    ("Four of Wands" . "Celebration, harmony, homecoming.")
+    ("Five of Wands" . "Conflict, competition, aggression.")
+    ("Six of Wands" . "Victory, recognition, progress.")
+    ("Seven of Wands" . "Perseverance, defensiveness, challenge.")
+    ("Eight of Wands" . "Swiftness, progress, movement.")
+    ("Nine of Wands" . "Resilience, courage, persistence.")
+    ("Ten of Wands" . "Burden, responsibility, hard work.")
+    ("Page of Wands" . "Inspiration, exploration, discovery.")
+    ("Knight of Wands" . "Energy, passion, adventure.")
+    ("Queen of Wands" . "Determination, confidence, creativity.")
+    ("King of Wands" . "Leadership, vision, entrepreneurship.")))
+
+(defvar minor-arcana-cards
+   (append swords-cards
+     wands-cards
+     cups-cards
+     pentacles-cards))
+
+(defvar major-arcana-cards
   '(("The Fool" . "A new beginning, taking a chance, trust in the journey.")
     ("The Magician" . "Manifestation, skill, power, action.")
     ("The High Priestess" . "Intuition, mystery, secrets.")
@@ -47,6 +117,17 @@
     ("The World" . "Completion, fulfillment, journey's end."))
   "List of tarot cards and their basic explanations.")
 
+(defvar tarot-cards
+  (append major-arcana-cards minor-arcana-cards))
+
+
+(defun tarot-card-message ()
+  "Selects a random tarot card from the `tarot-cards` list and returns a message."
+  (let ((card (nth (random (length tarot-cards)) tarot-cards)))
+    (format "Your tarot card for today is:\n%s\n%s" (car card) (cdr card))))
+
+(message (tarot-card-message))
+
 (defun tarot-card ()
   "Return a random tarot card with a basic explanation."
   (let* ((card (nth (random (length tarot-cards)) tarot-cards))
@@ -54,5 +135,5 @@
          (explanation (cdr card)))
     (format "%s: %s" name explanation)))
 
-(provide 'tarot-card)
+(provide 'tarock)
 ;;; tarock.el ends here
